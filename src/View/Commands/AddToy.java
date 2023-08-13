a896ae19aeaa59eb24e077c547308a553f175341
Artyom <artyom_kudryavcev_91@mail.ru>
2023-08-13 18:25:07 +0700

    Создал класс команды добавления игрушки

---
+++ src/View/Commands/AddToy.java
@@ -0,0 +1,11 @@
+package View.Commands;
+
+import View.View;
+
+public class AddToy extends Command{
+    private String description = "Добавить игрушку";
+    public AddToy(View view) {
+        super(view);
+    }
+
+}
