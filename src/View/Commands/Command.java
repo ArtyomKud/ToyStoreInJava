a0baefae99e71ee82c2ed96ffbe514b18b071dba
Artyom <artyom_kudryavcev_91@mail.ru>
2023-08-13 18:16:11 +0700

    Создал класс команд

---
+++ src/View/Commands/Command.java
@@ -0,0 +1,24 @@
+package View.Commands;
+
+import View.View;
+
+
+public class Command {
+    private String description;
+    private View view;
+
+    public Command(View view){
+        this.view = view;
+    }
+    public View getView(){
+        return view;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private void execute(){
+
+    }
+}
